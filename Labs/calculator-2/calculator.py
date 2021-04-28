9052c79a276ffa72f5a6524643e9ab3e325bcf24
Rachel Bates <rachelbatesnj@gmail.com>
2021-04-28 22:09:06 +0000

    Finished remaining function operations
    Co-authored by Kavya Nagarajan <kavya.0219@gmail.com>

--- Labs/calculator-2/calculator.py
+++ Labs/calculator-2/calculator.py
@@ -13,23 +13,39 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True :
     user_equation = input('Enter Your Equation. >')
     tokens = user_equation.split(' ')
-    tokens[1] = int(tokens[1])
-    tokens[2] = int(tokens[2])
+    
+  #  if tokens[1] != None :
+   #     tokens[1] = float(tokens[1])
+  #  if tokens[2] != None :
+  #      tokens[2] = float(tokens[2])
+
     if tokens[0].lower() == 'q' :
         break
     #Addition
     elif tokens[0] == '+' : 
-        print(add(tokens[1], tokens[2]))
+        print(add(float(tokens[1]), float(tokens[2])))
     #Subtraction
     elif tokens[0] == '-':
-        print(subtract(tokens[1], tokens[2]))
+        print(subtract(float(tokens[1]), float(tokens[2])))
     #Multiply
     elif tokens[0] == '*':
-        print(multiply(tokens[1], tokens[2]))
+        print(multiply(float(tokens[1]), float(tokens[2])))
 #Divide
     elif tokens[0] == '/':
-        print(divide(tokens[1], tokens[2]))
-
-
+        print(divide(float(tokens[1]), float(tokens[2])))
+#Square
+    elif tokens[0].lower() == 'square' :
+        print(square(float(tokens[1]))) 
+    #Cube
+    elif tokens[0].lower() == 'cube':
+        print(cube(float(tokens[1])))
+    #Power
+    elif tokens[0].lower() == 'power':
+        print(power(float(tokens[1]), float(tokens[2])))
+    #Mod
+    elif tokens[0].lower() == 'mod':
+        print(mod(float(tokens[1]), float(tokens[2])))
+    else:
+        print("Please enter an operator followed by two integers.")
     
 
